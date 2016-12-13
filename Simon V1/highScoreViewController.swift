b6e581b6f3a0851e316bfd6e8fb5ced581a5bb15
Balgard <student@balgard.local>
2016-12-13 12:43:04 -0600

    Got Color Showing for new score.   Need to fill leader-board with values.

--- Simon V1/highScoreViewController.swift
+++ Simon V1/highScoreViewController.swift
@@ -9,18 +9,7 @@
 import UIKit
 
 class highScoreViewController: UIViewController {
-    
-   /* @IBOutlet weak var name1: UILabel!
-    @IBOutlet weak var name2: UILabel!
-    @IBOutlet weak var name3: UILabel!
-    @IBOutlet weak var name4: UILabel!
-    @IBOutlet weak var name5: UILabel!
-    @IBOutlet weak var name6: UILabel!
-    @IBOutlet weak var name7: UILabel!
-    @IBOutlet weak var name8: UILabel!
-    @IBOutlet weak var name9: UILabel!
-    @IBOutlet weak var name10: UILabel!
- */
+
     @IBOutlet weak var score1: scoreLabel!
     @IBOutlet weak var score2: scoreLabel!
     @IBOutlet weak var score3: scoreLabel!
@@ -51,17 +40,7 @@ class highScoreViewController: UIViewController {
         scoreArray.append(score8)
         scoreArray.append(score9)
         scoreArray.append(score10)
-        /*nameArray.append(name1.text!)
-        nameArray.append(name2.text!)
-        nameArray.append(name3.text!)
-        nameArray.append(name4.text!)
-        nameArray.append(name5.text!)
-        nameArray.append(name6.text!)
-        nameArray.append(name7.text!)
-        nameArray.append(name8.text!)
-        nameArray.append(name9.text!)
-        nameArray.append(name10.text!)
-         */
+        
         setNewValues(label: scoreArray[2], value: 0)
         setNewValues(label: scoreArray[3], value: 0)
         setNewValues(label: scoreArray[1], value: 0)
@@ -75,27 +54,8 @@ class highScoreViewController: UIViewController {
         
         
         
-        /*name3.text = "X"
-        namesScores[score3.value] = "X"
-        nameArray[2] = "X"
-        */
-        
         
         numberArray()
-        
-        /*
-        namesScores[score1.value] = name1.text!
-        namesScores[score2.value] = name2.text!
-        namesScores[score3.value] = name3.text!
-        namesScores[score4.value] = name4.text!
-        namesScores[score5.value] = name5.text!
-        namesScores[score6.value] = name6.text!
-        namesScores[score7.value] = name7.text!
-        namesScores[score8.value] = name8.text!
-        namesScores[score9.value] = name9.text!
-        namesScores[score10.value] = name10.text!
-         */
-        
         
         
         sortValues()
@@ -130,6 +90,17 @@ class highScoreViewController: UIViewController {
             setNewValues(label: scoreArray[i], value: scores[i])
             i += 1
         }
+        for label in scoreArray
+        {
+            if label.value == score
+            {
+                label.backgroundColor = UIColor.yellow
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 10, execute: {
+                    label.backgroundColor = UIColor.clear
+                })
+            }
+        }
     }
     
     func sortValues()
@@ -150,25 +121,7 @@ class highScoreViewController: UIViewController {
         }
     }
     
-    /*func sortNames()
-    {
-        var oldScores = [Int]()
-        for (key, value) in namesScores
-        {
-            oldScores.append(key)
-        }
-        var y = 0
-        while y < scoreArray.count
-        {
-            if(oldScores[y] != scoreArray[y].value)
-            {
-                namesScores[y] = nameArray[y]
-            }
-            y += 1
-        }
-        //does nothing, but doesn't break anything
-    }*/
-    
+        
     func setNewValues(label : scoreLabel, value : Int)
     {
         label.value = value
